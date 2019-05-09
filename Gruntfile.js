10690d0100cd9ed847b8e57ca5d831cee7b223d2
Dieter <dieter@yoast.com>
2019-05-09 14:35:17 +0200

    task 2 finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,10 @@ module.exports = function(grunt) {
       main: {
         files: [
           // includes files within path
-          {expand: true, cwd: 'source-files/', src: ['**'], dest: 'temp/assets/', filter: 'isFile'},
-     
-          // includes files within path and its sub-directories
-          {expand: true, src: ['index.html'], dest: 'temp/'},
+          { expand: true, cwd: 'source-files/', src: ['*/**'], dest: 'temp/assets/', filter: 'isFile' },
+          { expand: true, cwd: 'source-files/', src: [ 'index.php' ], dest: 'temp/', filter: 'isFile' }
+          //index must not be moved to assets but remain in root. 
+
         ],
       },
     },
@@ -30,11 +30,22 @@ module.exports = function(grunt) {
     compress: {
       main: {
         options: {
-          archive: 'zips/task2.zip'
+          archive: 'zips/artifact.zip'
         },
         files: [
           {src: ['temp/**', "!**/*.scss"]},
         ]
+      }
+    },
+    babel: {
+      options: {
+        presets: ["minify"]
+      },
+      dist: {
+        files: {
+          'temp/assets/js/script.js': 'temp/assets/js/script.js', //dest: src
+          'temp/assets/js/conditional-script.js': 'temp/assets/js/conditional-script.js'
+        }
       }
     }
   });
@@ -43,10 +54,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-compress');
-
+  grunt.loadNpmTasks('babel-preset-minify');
+  grunt.loadNpmTasks('grunt-babel');
+  
   // Define aliases here.
   grunt.registerTask('default', 'My default task description', function() {
     grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
   });
-  grunt.registerTask('task2', ['copy','sass','compress']);
+  grunt.registerTask('artifact', ['copy','sass', 'babel', 'compress']);
 };
